2cf2e8bf766c180a26197838cba242e99d2b4e4e
Henrico Bela <henrico.developer@gmail.com>
2022-06-20 19:48:13 -0300

    First commit

---
+++ Django/tutorialdjango/blog/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import DetailView, ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+
