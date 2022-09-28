a58671bf72effb967840c9b09bd13f6837a57cb5
Henrico Bela <henrico.developer@gmail.com>
2022-09-28 09:42:03 -0300

    Update CameraWebServer.ino

--- Faculdade/I.A/NEXT/CameraWebServer/CameraWebServer.ino
+++ Faculdade/I.A/NEXT/CameraWebServer/CameraWebServer.ino
@@ -33,8 +33,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char* ssid = "Wireless_Room";
-const char* password = "Fmjs8544@";
+const char* ssid = "iPhoneHenrico";
+const char* password = "abc123456.";
 
 void startCameraServer();
 
