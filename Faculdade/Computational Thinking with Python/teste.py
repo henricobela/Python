ff024ea9ea9078713741ec8ae240fb66a266b890
Henrico Bela <henrico.developer@gmail.com>
2022-10-03 22:04:27 -0300

    Update teste.py

---
+++ Faculdade/Computational Thinking with Python/teste.py
@@ -0,0 +1,22 @@
+def menu():
+    print("""
+     ------ CADASTRO DE CLIENTES ------
+    |                                  |
+    |      1 - Criar cliente           |
+    |      2 - Listar clientes         |
+    |      3 - Editar cliente          |
+    |      4 - Excluir cliente         |
+    |      5 - Pesquisar cliente       |
+    |      6 - Sair                    |
+    |                                  |
+     ----------------------------------
+    """)
+
+
+def clear():
+    print("\n"*125)
+
+
+clear()
+menu()
+
