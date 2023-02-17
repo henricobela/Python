28202372c894ef25a8afe0fe651ca4a8e2d14301
henricobela <henrico.developer@gmail.com>
2023-02-17 19:57:01 -0300

    metricas

--- Faculdade/FIAP/Segundo_ano/Automated ML and Deploy/app.py
+++ Faculdade/FIAP/Segundo_ano/Automated ML and Deploy/app.py
@@ -4,9 +4,7 @@ import time
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    for i in range(100):
-        time.sleep(1)
-        col1.success("Coluna {} sucesso!!!".format(i))
+    col1.success("Coluna sucesso!!!")
 
 with col2:
     col2.error("Coluna 2 Error!!!")
