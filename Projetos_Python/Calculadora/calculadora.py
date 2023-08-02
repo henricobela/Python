4ee2dc2feb501772111a2d52808761f92316d466
henricobela <henrico.developer@gmail.com>
2023-08-02 16:48:49 -0300

    calculadora simples escolha

---
+++ Projetos_Python/Calculadora/calculadora.py
@@ -0,0 +1,12 @@
+print("-"*30)
+print("Calculadora Simples")
+print("-"*30)
+print("#"*30)
+print("Escolha uma operação Matemática")
+print("-"*30)
+
+operacoes = "1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\nEscolha um numero para operação matemática: "
+
+escolha = str(input(operacoes))
+
+print(f"Você escolheu: {escolha}")
