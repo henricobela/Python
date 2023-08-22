264f8aae4d51522dbedb972fcd0b73dca54598e7
henricobela <henrico.developer@gmail.com>
2023-08-22 10:11:18 -0300

    teste commit

--- Faculdade/FIAP/Segundo_ano/2o_semestre/NEUROMORFICA/kubernets/app.py
+++ Faculdade/FIAP/Segundo_ano/2o_semestre/NEUROMORFICA/kubernets/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    message = "Hello, this is a simple Flask app!"
+    message = "Hello, this is a simple Flask app!!!"
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
